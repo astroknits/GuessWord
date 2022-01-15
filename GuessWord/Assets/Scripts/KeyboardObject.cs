632c12ae4c91342bcc1e6abcd16d4f1da864cb22
Laura Kasian <laura.kasian@gmail.com>
2022-01-14 23:59:38 -0800

    tweaks to keyboard layout

--- GuessWord/Assets/Scripts/KeyboardObject.cs
+++ GuessWord/Assets/Scripts/KeyboardObject.cs
@@ -21,30 +21,49 @@ class KeyboardObject : BaseLetterGrid
         base(letterBox, letterBoxParent, gridQuad, gridMargin, cellPadding)
     {
         m_KeyboardArrangement = arrangement;
-        m_MaxCols = 9; // maximum 9 chars per row in keyboard
+        m_MaxCols = 10; // maximum 9 chars per row in keyboard
         m_MaxRows = 3; // maximum 3 rows per keyboard
-        SetUpKeyboard();
+        Create();
     }
 
-    internal void SetUpKeyboard()
+    internal void Create()
     {
-        float cellSize = 0.250f;
-        float xValue = -2.0f; // m_KeyboardQuad.transform.position.x - cellSize;
-        float yValue = -2.5f; // m_KeyboardQuad.transform.position.y + cellSize;
         m_Keyboard = new Dictionary<char, KeyboardCell>();
-        for (char letter = 'A'; letter <= 'Z'; letter++)
-        {
 
-            Vector3 centre = new Vector3(xValue, yValue, 4.0f);
+        float cellSize = GetCellSize();
+        Vector3 upperLeftCorner = GetUpperLeftCorner();
+
+        char[] alpha = GetCharArray();
+        float cellCentreXStart = upperLeftCorner.x + m_GridMargin + cellSize / 2.0f;
+        float cellCentreX = cellCentreXStart;
+        float cellCentreY = upperLeftCorner.y - (m_GridMargin + cellSize / 2.0f);
+        for (int l = 0; l < alpha.Length; l++)
+        {
+            char letter = alpha[l];
+            Vector3 centre = new Vector3(cellCentreX, cellCentreY, 9.5f);
             m_Keyboard.Add(letter, new KeyboardCell(letter, m_LetterBoxParent));
             m_Keyboard[letter].ConfigureCell(centre, cellSize, m_LetterBox);
             m_Keyboard[letter].RenderLetter(letter.ToString(), 3);
-            if (letter == 'I' || letter == 'R')
+            cellCentreX += (m_CellPadding + cellSize);
+            if (letter == 'P' || letter == 'L')
             {
-                yValue -= cellSize + 0.09f;
-                xValue = -2.0f;
+                cellCentreY -= (m_CellPadding + cellSize);
+                cellCentreX = cellCentreXStart;
             }
-            xValue += cellSize + 0.09f;
+        }
+    }
+    internal char[] GetCharArray()
+    {
+        switch (m_KeyboardArrangement)
+        {
+            case KeyboardArrangement.ALPHA:
+                return "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+            case KeyboardArrangement.QWERTY:
+                return "QWERTYUIOPASDFGHJKLZXCVBNM".ToCharArray();
+
+            default:
+                return "QWERTYUIOPASDFGHJKLZXCVBNM".ToCharArray();
         }
     }
 }
